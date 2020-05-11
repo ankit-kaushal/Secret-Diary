3a109b58125f13cafd0ef9e95848b15b11a92fce
Ankit Kaushal <55594118+ankit-kaushal@users.noreply.github.com>
2020-05-11 19:45:29 +0530

    Initial Commit

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $link = mysqli_connect("localhost", "id13619058_secretd", "@Nkit16181618", "id13619058_secretdiary");
+
+		if(mysqli_connect_error()){
+
+    		die ("Database Connection Error!!");
+
+    	}
+
+?>
---
+++ header.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="shortcut icon" href="favicon.png">
+    <title>Secret Diary</title>
+    <style type="text/css">
+
+      html { 
+        background: url(background.jpg) no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+      }
+
+    body{
+      background: none;
+    }
+      
+      .container{
+        text-align: center;
+        width: 400px;
+      }
+
+      #homepage{
+        margin-top: 150px;
+      }
+
+      #containerLoggedInPage{
+        margin-top: 60px;
+      }
+
+      #loginform{
+        display: none;
+      }
+
+      .toggleForms{
+        font-weight: bold;
+        cursor: pointer;
+      }
+
+      #diary{
+        width: 100%;
+        height: 100%;
+      }
+
+
+    </style>
+
+
+  </head>
+  <body>
